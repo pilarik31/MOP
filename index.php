cf9f88278f1203a510447a0e679638a1ea736c59
John Pilar <pilarjan4111@gmail.com>
2020-01-20 22:44:20 +0100

    Help me
    Co-authored-by: karolinamaresova <maresova.karolina@gmail.com>

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 include_once  "header.php";
 include_once  "nav.php";
 session_start();
-var_dump ($_SESSION["userRole"]);
+var_dump($_SESSION["userRole"]);
 ?>
  
   
- <h3>  Jsi přihlášen jako <?php echo  ($_SESSION["userRole"]) ?> </h3>
+ <h3>Jsi přihlášen jako <?php echo  ($_SESSION["userRole"]) ?> </h3>
  
--- rides.php
+++ rides.php
@@ -2,117 +2,107 @@
 include_once  "header.php";
 include_once "nav.php";
 session_start();
-if (!($_SESSION["userRole"]=="1" || $_SESSION["userRole"]=="4")) {
+if (!($_SESSION["userRole"] == "1" || $_SESSION["userRole"] == "4")) {
     header("location:index.php");
 }
 ?>
+<br><br>
+<h3>Rides</h3>
+<a class="btn btn-secondary background-btn" href="addRide.php">Add ride</a>
+<div class="table-responsive">
+    <?php if ($_SESSION["userRole"] == "1") {
+            $rides = Model::getAllRides(); ?>
 
-<body>
-    <br><br>
-    <h3>Rides</h3>
-    <a class="btn btn-secondary background-btn" href="addRide.php">Add ride</a>
-    <div class="table-responsive">
-        <?php if ($_SESSION["userRole"]=="1") {
-    $rides = Model::getAllRides(); ?> 
-
-        <table class=" table table-striped ">
-            <thead>
-                <tr>
-                    <th>id</th>
-                    <th>car</th>
-                    <th>TL</th>
-                    <th>TA</th>
-                    <th>PL</th>
-                    <th>PA</th>
-                    <th>km</th>
-                    <th>km</th>
-                    <th>note</th>
-                    <th>edit</th>
-
-
-                </tr>
-            </thead>
-            <tbody>
-                <?php
-foreach ($rides as $ride) {
-        ?><tr>
-
-                    <td><?php echo $ride['id_ride'] ?></td>
-
-                    <td><?php echo $ride['id_car'] ?></td>
-                    <td><?php echo $ride['time_left'] ?></td>
-                    <td><?php echo $ride['time_arrived'] ?></td>
-                    <td><?php echo $ride['place_left'] ?></td>
-                    <td><?php echo $ride['time_arrived'] ?></td>
-                    <td><?php echo $ride['km_before'] ?></td>
-                    <td><?php echo $ride['km_after'] ?></td>
-                    <td><?php echo $ride['note'] ?></td>
-                    <td>
-                        <a href="edit_ride.php?id_ride=<?= $ride['id_ride'] ?>">edit </a>
-                    </td>
-
-
-
-
-        </tr>
-        <?php
-    } ?></tbody>
-    </table> <?php
-    
-} else {
-    var_dump($_SESSION);
-    $driverRides = Model::getAllDriverRides(); 
-    var_dump($driverRides);
-    ?>
-    
     <table class=" table table-striped ">
-                <thead>
-                    <tr>
-                        <th>id</th>
-                        <th>car</th>
-                        <th>TL</th>
-                        <th>TA</th>
-                        <th>PL</th>
-                        <th>PA</th>
-                        <th>km</th>
-                        <th>km</th>
-                        <th>note</th>
-                        <th>edit</th>
-    
-    
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php
-    foreach ($driverRides as $driverRide) {
-        ?><tr>
-    
-                        <td><?php echo $driverRide['id_ride'] ?></td>
-                        <td><?php echo $driverRide['id_car'] ?></td>
-                        <td><?php echo $driverRide['time_left'] ?></td>
-                        <td><?php echo $driverRide['time_arrived'] ?></td>
-                        <td><?php echo $driverRide['place_left'] ?></td>
-                        <td><?php echo $driverRide['time_arrived'] ?></td>
-                        <td><?php echo $driverRide['km_before'] ?></td>
-                        <td><?php echo $driverRide['km_after'] ?></td>
-                        <td><?php echo $driverRide['note'] ?></td>
-                        
-                        <td>
-                            <a href="edit_ride.php?id_ride=<?= $ride['id_ride'] ?>">edit </a>
-                        </td>
-    
-    
-    
-    
-                    </tr>
-                
+        <thead>
+            <tr>
+                <th>id</th>
+                <th>car</th>
+                <th>TL</th>
+                <th>TA</th>
+                <th>PL</th>
+                <th>PA</th>
+                <th>km</th>
+                <th>km</th>
+                <th>note</th>
+                <th>edit</th>
+            </tr>
+        </thead>
+        <tbody>
             <?php
-    } ?>
-    </tbody>
-    </table>
-    <?php   
-}
-            ?>
-            
+            foreach ($rides as $ride) {
+                ?>
+                    <tr>
 
-    </div>
+                <td><?= $ride['id_ride'] ?></td>
+
+                <td><?= $ride['id_car'] ?></td>
+                <td><?= date("j.n.Y - G:i:s", strtotime($ride['time_left'])) ?></td>
+                <td><?= date("j.n.Y - G:i:s", strtotime($ride['time_arrived'])) ?></td>
+                <td><?= $ride['place_left'] ?></td>
+                <td><?= $ride['time_arrived'] ?></td>
+                <td><?= $ride['km_before'] ?></td>
+                <td><?= $ride['km_after'] ?></td>
+                <td><?= $ride['note'] ?></td>
+                <td>
+                    <a href="edit_ride.php?id_ride=<?= $ride['id_ride'] ?>">edit </a>
+                </td>
+
+
+
+
+            </tr>
+                <?php
+            } ?></tbody>
+    </table> <?php
+    } else {
+        $driverRides = Model::getAllDriverRides($_SESSION['userId']['id_user']);
+        ?>
+
+    <table class=" table table-striped ">
+        <thead>
+            <tr>
+                <th>id</th>
+                <th>car</th>
+                <th>TL</th>
+                <th>TA</th>
+                <th>PL</th>
+                <th>PA</th>
+                <th>km</th>
+                <th>km</th>
+                <th>note</th>
+                <th>edit</th>
+
+
+            </tr>
+        </thead>
+        <tbody>
+        <?php
+        foreach ($driverRides as $driverRide) {
+            ?><tr>
+
+                <td><?= $driverRide['id_ride'] ?></td>
+                <td><?= $driverRide['id_car'] ?></td>
+                <td><?= date("j.n.Y - G:i:s", strtotime($driverRide['time_left'])) ?></td>
+                <td><?= date("j.n.Y - G:i:s", strtotime($driverRide['time_arrived'])) ?></td>
+                <td><?= $driverRide['place_left'] ?></td>
+                <td><?= $driverRide['time_arrived'] ?></td>
+                <td><?= $driverRide['km_before'] ?></td>
+                <td><?= $driverRide['km_after'] ?></td>
+                <td><?= $driverRide['note'] ?></td>
+
+                <td>
+                    <a href="edit_ride.php?id_ride=<?= $driverRide['id_ride'] ?>">edit </a>
+                </td>
+            </tr>
+
+            <?php
+        } ?>
+        </tbody>
+    </table>
+        <?php
+    }
+    ?>
+
+
+</div>
